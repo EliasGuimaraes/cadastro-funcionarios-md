8b4a12d0e25ad356118678b4b3c531bc0ba8b6ef
Elias Guimarães <elias.mg1997@gmail.com>
2021-05-13 04:59:52 -0300

    register publication in admin.py

--- registermd/admin.py
+++ registermd/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Publication
 
 # Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('nome', 'cargo', 'publication_date')
+    list_filter = ('nome', 'cargo')
+    search_fields = ('nome', 'cargo')
+
+
+admin.site.register(Publication, PublicationAdmin)
