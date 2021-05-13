5cde36c93cc1f1b4b63a5b5ad9221473e6b5f5c6
Elias Guimarães <elias.mg1997@gmail.com>
2021-05-13 04:41:42 -0300

    Add publication model for registermd

--- registermd/models.py
+++ registermd/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+    regiao = models.TextField()
+    pais = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200)
+    cargo = models.CharField(max_length=200)
+    time = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    telefone = CharField(max_length=200)
+    cpf = CharField(max_length=200)
+    rg = CharField(max_length=200)
+    data_de_nascimento = models.DateField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
