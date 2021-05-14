f10f01424b2f71a223bc123ac68905d6533e4af7
Elias Guimarães <elias.mg1997@gmail.com>
2021-05-14 03:36:04 -0300

    Make front-end

--- registermd/models.py
+++ registermd/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+regiao_choices = [
+    ('BRASIL', 'BRASIL'),
+    ('NORLA', 'NORLA'),
+    ('CONOSUR', 'CONOSUR')
+]
+
+
 # Create your models here.
 class Publication(models.Model):
-    regiao = models.TextField()
+    regiao = models.CharField(max_length=7, choices=regiao_choices, default = 'BRASIL')
     pais = models.CharField(max_length=200)
     nome = models.CharField(max_length=200)
     cargo = models.CharField(max_length=200)
