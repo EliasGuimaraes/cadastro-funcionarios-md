573dcf9535e56e2c2bcd2dccb18025c418010ba4
Elias Guimarães <elias.mg1997@gmail.com>
2021-05-14 03:58:18 -0300

    Create dropdownlist to time in models.py

--- registermd/models.py
+++ registermd/models.py
@@ -6,6 +6,17 @@ regiao_choices = [
     ('CONOSUR', 'CONOSUR')
 ]
 
+time_choices = [
+    ('A&KT', 'A&KT'),
+    ('AS&D, Integration', 'AS&D, Integration'),
+    ('AS & D, Integration', 'AS & D, Integration'),
+    ('All MD Team', 'All MD Team'),
+    ('Corn & Sorghum', 'Corn & Sorghum'),
+    ('Crop Protection', 'Crop Protection'),
+    ('MDR & FOE', 'MDR & FOE'),
+    ('Soybean & Cotton', 'Soybean & Cotton'),
+]
+
 
 # Create your models here.
 class Publication(models.Model):
@@ -13,7 +24,7 @@ class Publication(models.Model):
     pais = models.CharField(max_length=200)
     nome = models.CharField(max_length=200)
     cargo = models.CharField(max_length=200)
-    time = models.CharField(max_length=200)
+    time = models.CharField(max_length=100, choices=time_choices, default = 'A&KT')
     email = models.CharField(max_length=200)
     telefone = models.CharField(max_length=200)
     cpf = models.CharField(max_length=200)
